af37d0ed6e53c4a59c5c306546ece907a97068e0
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-03 08:16:09 +0300

    webserver

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
 const publicPath = './public'
   console.log(req.url)
 
-  const body = req.url === `style.css` 
+  const body = req.url === `index.css` 
     ? fs.readFileSync(`${publicPath}/style.css` ,'utf8')
     : fs.readFileSync(`${publicPath}/index.html` ,'utf8')
 
