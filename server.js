cbb6ddb6cd2050c7e0dc5458de3fbf903bf39f2f
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-06 15:26:20 +0300

    webserver

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ const path = require('path');
 const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
-    
-     switch (path.extname(urlFile)){
+
+    if (path.extname(urlFile) = '.css'){
+        res.writeHead(200, {'Cotent-Type': 'text/css'});
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+    } else if (path.extname(urlFile) = '.js'){
+        res.writeHead(200, {'Cotent-Type': 'text/js'});
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+    } else if (path.extname(urlFile) = '.jpg'){
+        res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`));
+    } else {
+        res.writeHead(200, {'Cotent-Type': 'text/html'});
+        res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
+    }
+
+     /*switch (path.extname(urlFile)){
         case '.css':
             res.writeHead(200, {'Cotent-Type': 'text/css'});
             res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
@@ -23,5 +37,5 @@ const server = http.createServer((req, res) =>{
             res.writeHead(200, {'Cotent-Type': 'text/html'});
             res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
             break;
-     }
+     }*/
 }).listen(process.env.PORT || 3000);
