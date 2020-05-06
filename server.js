1f104a6cd777df098b54c049fa28c333aea695e5
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-06 15:28:19 +0300

    webserver

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
 
-    if (path.extname(urlFile) = '.css'){
+    if (path.extname(urlFile) == '.css'){
         res.writeHead(200, {'Cotent-Type': 'text/css'});
         res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    } else if (path.extname(urlFile) = '.js'){
+    } else if (path.extname(urlFile) == '.js'){
         res.writeHead(200, {'Cotent-Type': 'text/js'});
         res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    } else if (path.extname(urlFile) = '.jpg'){
+    } else if (path.extname(urlFile) == '.jpg'){
         res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
         res.end(fs.readFileSync(`${pathFile}${urlFile}`));
     } else {
