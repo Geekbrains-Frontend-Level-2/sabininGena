54ac04db711f6d051287196cc678d82db9be1657
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-07 16:03:11 +0300

    webserver

--- server.js
+++ server.js
@@ -5,23 +5,25 @@ const path = require('path');
 const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
-
+    function readFile(){
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+    }
 try {
     if (path.extname(urlFile) == '.css'){
         res.writeHead(200, {'Cotent-Type': 'text/css'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+        readFile();
     } else if (path.extname(urlFile) == '.js'){
         res.writeHead(200, {'Cotent-Type': 'text/js'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+        readFile();
     } else if (path.extname(urlFile) == '.jpg'){
         res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
         res.end(fs.readFileSync(`${pathFile}${urlFile}`));
     } else {
         res.writeHead(200, {'Cotent-Type': 'text/html'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+        readFile();
     }
 } catch {
     res.writeHead(200, {'Cotent-Type': 'text/html'});
-    res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+    res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
     }
 }).listen(process.env.PORT || 3000);
