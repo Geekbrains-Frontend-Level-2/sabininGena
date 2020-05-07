ac9ca7c92279de18882834d7ed261a480465d265
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-07 15:27:00 +0300

    webserver

--- server.js
+++ server.js
@@ -6,42 +6,27 @@ const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
 
-try {
-    if (path.extname(urlFile) == '.css'){
-        res.writeHead(200, {'Cotent-Type': 'text/css'});
+    function readFile(){
         res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    } else if (path.extname(urlFile) == '.js'){
-        res.writeHead(200, {'Cotent-Type': 'text/js'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    } else if (path.extname(urlFile) == '.jpg'){
-        res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`));
-    } else {
-        res.writeHead(200, {'Cotent-Type': 'text/html'});
-        res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
-    }
-} catch {
-    res.writeHead(200, {'Cotent-Type': 'text/html'});
-    res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
-    }
+    }    
 
-
-     /*switch (path.extname(urlFile)){
-        case '.css':
+    try {
+        if (path.extname(urlFile) == '.css'){
             res.writeHead(200, {'Cotent-Type': 'text/css'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-            break;
-        case '.js':
+            readFile();
+        } else if (path.extname(urlFile) == '.js'){
             res.writeHead(200, {'Cotent-Type': 'text/js'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-            break;
-        case '.jpg':
+            readFile();
+        } else if (path.extname(urlFile) == '.jpg'){
             res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`));
-            break;
-        default:
+            readFile();
+            //res.end(fs.readFileSync(`${pathFile}${urlFile}`));
+        } else {
             res.writeHead(200, {'Cotent-Type': 'text/html'});
-            res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
-            break;
-     }*/
+            readFile();
+        }
+    } catch {
+        res.writeHead(200, {'Cotent-Type': 'text/html'});
+        readFile();
+    }
 }).listen(process.env.PORT || 3000);
