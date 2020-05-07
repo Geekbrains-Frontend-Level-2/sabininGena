10b05e71d071ecd0e8d1a63d8a3316c281c1e56d
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-07 15:52:08 +0300

    webserver

--- server.js
+++ server.js
@@ -6,26 +6,22 @@ const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
 
-    /*function readFile(){
+try {
+    if (path.extname(urlFile) == '.css'){
+        res.writeHead(200, {'Cotent-Type': 'text/css'});
         res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    }   */ 
-
-    try {
-        if (path.extname(urlFile) == '.css'){
-            res.writeHead(200, {'Cotent-Type': 'text/css'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-        } else if (path.extname(urlFile) == '.js'){
-            res.writeHead(200, {'Cotent-Type': 'text/js'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-        } else if (path.extname(urlFile) == '.jpg'){
-            res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`));
-        } else {
-            res.writeHead(200, {'Cotent-Type': 'text/html'});
-            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-        }
-    } catch {
+    } else if (path.extname(urlFile) == '.js'){
+        res.writeHead(200, {'Cotent-Type': 'text/js'});
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+    } else if (path.extname(urlFile) == '.jpg'){
+        res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`));
+    } else {
         res.writeHead(200, {'Cotent-Type': 'text/html'});
-        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
+        res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
+    }
+} catch {
+    res.writeHead(200, {'Cotent-Type': 'text/html'});
+    res.end(fs.readFileSync(`${pathFile}/index.html`, 'utf-8'));
     }
 }).listen(process.env.PORT || 3000);
