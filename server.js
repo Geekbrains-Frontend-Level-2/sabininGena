535a5a0e99b80380e982a68ef62bee570f852e64
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-07 15:38:12 +0300

    webserver

--- server.js
+++ server.js
@@ -6,27 +6,26 @@ const server = http.createServer((req, res) =>{
     const pathFile = './public';
     const urlFile = req.url;
 
-    function readFile(){
+    /*function readFile(){
         res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
-    }    
+    }   */ 
 
     try {
         if (path.extname(urlFile) == '.css'){
             res.writeHead(200, {'Cotent-Type': 'text/css'});
-            readFile();
+            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
         } else if (path.extname(urlFile) == '.js'){
             res.writeHead(200, {'Cotent-Type': 'text/js'});
-            readFile();
+            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
         } else if (path.extname(urlFile) == '.jpg'){
             res.writeHead(200, {'Cotent-Type': 'image/jpeg'});
-            //readFile();
             res.end(fs.readFileSync(`${pathFile}${urlFile}`));
         } else {
             res.writeHead(200, {'Cotent-Type': 'text/html'});
-            readFile();
+            res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
         }
     } catch {
         res.writeHead(200, {'Cotent-Type': 'text/html'});
-        readFile();
+        res.end(fs.readFileSync(`${pathFile}${urlFile}`, 'utf-8'));
     }
 }).listen(process.env.PORT || 3000);
