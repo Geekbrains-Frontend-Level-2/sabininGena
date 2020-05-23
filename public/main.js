b7ae7b92010763b839d674eb0280c12e4805c644
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-23 18:47:03 +0300

    regularV

--- public/main.js
+++ public/main.js
@@ -208,7 +208,7 @@
 
         dec(){
           if(this._counter = 1){
-            this.del
+            this.del()
           } else{
             return Promise.resolve(this._counter--)
               .then(()=>{
@@ -231,10 +231,10 @@
             <span class='cart__item'>
             ${this._data.title} x ${this._counter} = ${this.totalPrice}            
             </span>
-            <span class='btnDelete'>x</span>
+            <div class='btnDelete btnDelete_${this._data.id}'>x</div>
           `
-          const btnDel = document.querySelector('.btnDelete')
-          //не понял почему не работает
+          const btnDel = document.querySelector('.btnDelete_' + this._data.id)
+          
           //btnDel.addEventListener('click', this.dec.bind(this))
         }
       }
@@ -244,24 +244,41 @@
 
       
       
-      
-      const regexp = /^([a-z]+)/i
-      const inputName = document.getElementById('inputName')
-      //const inputNameValue = document.getElementById('inputName').value
+      class Input {
+        constructor(id, data, root){
+          this._data = data
+          this._root = root
+          this._id = id
+          this.initTemplate()
+        }
 
-      inputName.addEventListener('change', () => {
-        console.log(regexp.test(document.getElementById('inputName').value))
-      })
+        initTemplate(){
+          this._template = document.getElementById('inputProblem' + this._id)
+          this._root.addEventListener('change', this.renderProblem.bind(this))
+        }
 
-      const regexp2 = /^[0-9\+\(\)\-]+/
-      const inputTel = document.getElementById('inputTelefone')
-      //const inputTelValue = document.getElementById('inputTelefone').value
-
-      inputTel.addEventListener('change', () => {
-        console.log(regexp2.test(document.getElementById('inputTelefone').value))
-      })
-    
-      
-      
-
+        renderProblem(){
+            if(!this._data.test(this._root.value)){
+              this._template.innerHTML = '! Некорректный ввод'       
+            }
+            if(this._data.test(this._root.value)){
+              this._template.innerHTML = ''    
+            }
+          }
         
+      }
+      
+      const i1 = {
+          id: 1,
+          reg: /^([a-z]+)/i,
+          root: document.getElementById('inputName')
+        }
+      const i2 =  {
+          id: 2,
+          reg: /^((\+7)|8)[\(]?([0-9]{3,4})[\)]?([0-9]{3})[\-]?([0-9]{4})/,
+          root: document.getElementById('inputTelefone')
+        }
+
+      const InpName = new Input(i1.id, i1.reg, i1.root)
+      const InpTel = new Input(i2.id, i2.reg, i2.root)
+      
