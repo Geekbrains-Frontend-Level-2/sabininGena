fc1e6bd3b15d2951313c50549cf5f0a3ea2b0db0
Gennadiy Sabinin <gsabinin@icloud.com>
2020-05-28 13:52:47 +0300

    VUE

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,137 @@
 <template>
-  
+  <div>
+
+      <header :class="[$style.header]">
+    <span>IceHockey shop!</span> 
+  </header>
+  <div :class="[$style.cart]">
+    <span :class="[$style.cartLogo]">Корзина</span> 
+    </div>
+    <main>
+      <div>
+      <h1>Список товаров</h1>
+      </div>
+       <Item
+        v-for="(item, index) in items"
+        :key="index"
+         :title="item.title" :price="item.price" :src="item.src"
+        />
+    </main>
+    <form action="form">
+      <input type="text" placeholder="Ваше имя: " id="inputName">
+      <span id="inputProblem1" class="inpP"></span>
+      <input type="tel" placeholder="+7(999)999-7755" id="inputTelefone">
+      <span id="inputProblem2" class="inpP"></span>
+      <input type="email" placeholder="my.mail@mail.ru" id="inputMail">
+      <span id="inputProblem3" class="inpP"></span>
+      <input type="submit" class="submit">
+      <input type="reset" class="reset">
+    </form>
+
+  </div>
+
 </template>
 
 <script>
-export default {
+import Item from './Item.vue'
 
+export default {
+    data() {
+        return {
+            items: [
+      { "id": 1, "title": "Коньки", "price": 30000, "src": "img/1.jpg" },
+      { "id": 2, "title": "Нагрудник", "price": 5000, "src": "img/11.jpg" },
+      { "id": 3, "title": "Краги", "price": 3500, "src": "img/12.jpg" },
+      { "id": 4, "title": "Шлем", "price": 2500, "src": "img/13.jpg" }
+    ]
+        }
+    },
+
+    components: {
+        Item,
+    }    
 }
 </script>
 
-<style>
+<style module>
+
+    .header {
+    background-color: rgb(235, 94, 13);
+    color: rgb(233, 231, 124);
+    font-size: 50px;
+}
+
+.cart {
+    background-color: rgb(8, 41, 185);
+    color: rgb(233, 231, 124);
+    font-size: 40px;
+    margin-top: -58px;
+    float: right;
+}
+
+.CartItems {
+    font-size: 22px;
+    background-color: #fff;
+    color: black;
+}
+
+.cart__list {
+    background-color: #fff;
+    font-size: 15px;
+    color: blue;
+    display: none;
+}
+
+.cart__list.shown {
+    display: block;
+}
+
+.btnDelete {
+    color: #fff;
+    background-color: red;
+    width: 20px;
+    display: inline-block;
+    text-align: center;
+    font-size: large;
+}
+.btnDelete:hover {
+    cursor: pointer;
+}
+
+form {
+    margin-top: 50px;
+    width: 200px;
+}
+
+input{
+    display: block;
+    margin: 10px 0;
+}
+
+.submit,
+.reset{
+    display: inline-block;
+}
+
+.inpP{
+    color: red;
+    font-size: 10px;
+    display: block;
+}
+
+@media all and (max-width: 576px) {
+    .items-list {
+        grid-template-columns: 1fr;
+    }
+}
+@media (min-width: 576px) and (max-width: 992px) {
+    .items-list {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    
+}
+@media (min-width: 992px) {
+    
+}
 
 </style>
---
+++ src/Item.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="[$style.item]">
+
+            <div :class="[$style.item__img]">
+            <img :src="src" />
+            </div>
+            <div :class="[$style.item__meta]">Товар: <span>{{ title }}</span></div>
+            <div :class="[$style.item__meta]">Цена: <span>{{ price }}</span></div>
+            <!--<button class='buy'>Купить</button>-->
+
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        price: Number,
+        src: String
+    }
+}
+</script>
+
+<style module>
+.items-list {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    text-align: center;
+}
+
+.item {
+    width: 200px;
+    background-color: rgb(223, 206, 206);
+    margin: 30px;
+    display: inline-block;
+}
+.item:hover{
+    background-color: #fff;
+}
+.item__meta {
+    text-align: left;
+    margin-left: 30px;
+}
+img {
+    width: 100%;
+}
+
+button {
+    float: right;
+}
+</style>
